341e7eb443f758759714b37f793e397d54dad7c0
Opilane TTHK <opilane@tthk.ee>
2021-12-07 13:34:21 +0200

    Add project files.

---
+++ ExcersicePart1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExcersicePart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname, lname;
+            int year, age;
+
+            Console.WriteLine("Hi, enter your name!");
+            fname = Console.ReadLine();
+            Console.WriteLine("And your last name :)");
+            lname = Console.ReadLine();
+            Console.WriteLine($"Hello,{fname},{lname}");
+            Console.WriteLine("Enter your age ");
+            year = Convert.ToInt32(Console.ReadLine());
+            int ConvertDate = DateTime.Now.Year;
+            age = ConvertDate - year;
+
+            if (age >= 10)
+            {
+                Console.WriteLine("Welcome to Instagram!");
+            } else
+            {
+                Console.WriteLine("You are to young to have an Instagram account!");
+            }
+
+            
+            
+        }
+      
+    }
+}
---
+++ PersonalityTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userinput;
+            Console.WriteLine("Hello, enter your favorite color");
+            userinput = Console.ReadLine().ToLower();
+            string template = $"You picked {userinput}"
+            if (userinput == "red") ;
+            {
+                Console.WriteLine($"{template}, You are romantic!");
+            }
+            else if (userinput == "blue")
+            {
+                Console.WriteLine($"{template}, You are reliable!");
+            }
+            else if (userinput == "yellow")
+            {
+                Console.WriteLine($"{template}, You are reliable!");
+            }
+            else
+            {
+
+                Console.WriteLine($"{template}, You are a/an {userinput} unicorn!");
+
+            }
+    }
+}
